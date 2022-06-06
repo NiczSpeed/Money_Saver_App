e2870e1071a0361afc7224f9d21be4185c7684f6
Mateusz Libera <mateusz.libera01@gmail.com>
2022-06-06 11:52:25 +0200

    Initial commit

---
+++ src/main/java/pl/niczspeed/money_saver_app/MoneySaverAppApplication.java
@@ -0,0 +1,13 @@
+package pl.niczspeed.money_saver_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneySaverAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneySaverAppApplication.class, args);
+    }
+
+}
